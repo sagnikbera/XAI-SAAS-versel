7e8a148bee3c051c8bc4297e348d31c88957773a
sagnikbera <sagnikberaofficial@gmail.com>
2025-08-09 00:27:29 +0530

    Hero Section - Landing page

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Hero = () => {
+  return (
+    <div
+      className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-cover bg-no-repeat min-h-screen"
+      style={{ backgroundImage: `url(${assets.gradientBackground})` }}
+    >
+      <div className="text-center mb-6">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-bold x-auto leading-[1.2]">
+          Create amazing content <br /> with{" "}
+          <span className="text-[var(--pmcol)]">AI</span> tools
+        </h1>
+        <p className="mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600">
+          Transform you workflow with{" "}
+          <span className="font-semibold">AI tools</span>,{" "}
+          <span className="font-semibold">Write Articles</span> ,{" "}
+          <span className="font-semibold">Generate Images</span>,{" "}
+          <span className="font-semibold">Remove Background</span> ,{" "}
+          <span className="font-semibold">Remove Object</span> etc.
+        </p>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs">
+        <button
+        className="bg-[var(--pmcol)] font-semibold text-black px-10 py-3 rounded-lg hover:bg-black hover:text-[var(--pmcol)] hover:scale-102 active:scale-95 transition cursor-pointer"
+        >Start Creating Now</button>
+        <button
+        className="bg-white px-10 font-semibold py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer hover:text-white hover:bg-black"
+        >Watch Demo</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,27 +3,37 @@ import { FaInfinity, FaAt } from "react-icons/fa";
 import { assets } from "./../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+
   return (
-    <div className="fixed z-50 w-full backdrop-blur-2xl flex justify-between items-center py-2 px-4 sm:px-16 xl:px-28">
+    <div className="fixed z-50 w-full backdrop-blur-2xl flex justify-between items-center py-4 px-4 sm:px-16 xl:px-28">
       <img
         src={assets.logo}
         alt="logo"
-        className="w-28 sm:w-36 cursor-progress"
+        className="w-20 sm:w-28 cursor-progress"
         onClick={() => navigate("/")}
       />
 
-      <button
-        className="flex items-center gap-2 rounded-full text-lg cursor-pointer 
+      {user ? (
+        <span className="scale-200 origin-center m-2 mt-2 px-2"><UserButton/></span>
+      ) : (
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 rounded-full text-lg cursor-pointer 
              bg-[var(--pmcol)] text-black px-8 py-2
              font-semibold transition-colors duration-300
              hover:bg-black hover:text-[var(--pmcol)]"
-      >
-        <span>Get Started</span>
-        <ArrowRight className="w-4 h-4" />
-      </button>
+        >
+          <span>Get Started</span>
+          <ArrowRight className="w-4 h-4" />
+        </button>
+      )}
     </div>
   );
 };
